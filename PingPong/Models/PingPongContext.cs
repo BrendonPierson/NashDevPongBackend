3c6fa9461ac8c84489c8de23997425e9a885d7d4
Brendon Pierson <brendonpierson@gmail.com>
2015-12-12 14:19:27 -0600

    Added pingpong context

---
+++ PingPong/Models/PingPongContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace PingPong.Models
+{
+    public class PingPongContext : ApplicationDbContext
+    {
+        public virtual DbSet<Player> Players { get; set; }
+        public virtual DbSet<DoublesTeam> Teams { get; set; }
+        public virtual DbSet<SinglesMatch> SinglesMatches { get; set; }
+        public virtual DbSet<DoublesMatch> DoublesMatches { get; set; }
+        public virtual DbSet<SinglesTournament> SinglesTournament { get; set; }
+        public virtual DbSet<DoublesTournament> DoublesTournament { get; set; }
+    }
+}
